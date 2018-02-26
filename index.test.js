98e5603756d03dc55b65753cbf5fbe347eea945c
Alan Fachini <alfakini@gmail.com>
2018-02-25 22:54:59 -0300

    Update Jest syntax

--- index.test.js
+++ index.test.js
@@ -1,5 +1,7 @@
 import sum from './index';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('sum', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toEqual(3);
+  });
 });
